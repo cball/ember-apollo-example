b13fe7c1b48f0bcd4f2e04bb9696a42966ceda4a
Chris Ball <chris@echobind.com>
2017-11-09 13:46:20 -0500

    Move create logic to component

--- app/components/game-form/component.js
+++ app/components/game-form/component.js
@@ -1,3 +1,54 @@
 import Component from '@ember/component';
+import ComponentQueryManager from 'ember-apollo-client/mixins/component-query-manager';
+import { createRoundMutation, createGameMutation } from './mutations';
 
-export default Component.extend({});
+export default Component.extend(ComponentQueryManager, {
+  isCreatingGame: false,
+
+  actions: {
+    createGame: async function(attrs = {}) {
+      this.set('isCreatingGame', true);
+
+      const round = await this._createRound(attrs);
+      await this._createGame(round);
+
+      this.set('isCreatingGame', false);
+      this.onCreateGame();
+    }
+  },
+
+  _createRound: async function({ emojiPhrase }) {
+    const apollo = this.get('apollo');
+
+    const variables = {
+      emojiPhrase,
+      userId: this.currentUser.id
+    };
+
+    const roundResult = await apollo.mutate({
+      mutation: createRoundMutation,
+      variables
+    });
+
+    return roundResult.createRound;
+  },
+
+  _createGame: async function(round) {
+    const apollo = this.get('apollo');
+
+    const gameResult = await apollo.mutate({
+      mutation: createGameMutation,
+      variables: {
+        roundId: round.id,
+        userId: this.currentUser.id
+      }
+    });
+
+    return gameResult.createGame;
+  },
+
+  // typically we would fetch this from a currentUser service
+  currentUser: {
+    id: 'cj7c07dmx2mz70110ju7rb8dr'
+  }
+});
--- app/new-game/route.js
+++ app/new-game/route.js
@@ -1,8 +1,6 @@
 import Route from '@ember/routing/route';
-import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
-import { createRoundMutation, createGameMutation } from './mutations';
 
-export default Route.extend(RouteQueryManager, {
+export default Route.extend({
   model() {
     return {
       emojiPhrase: ''
@@ -10,45 +8,8 @@ export default Route.extend(RouteQueryManager, {
   },
 
   actions: {
-    createGame: async function(attrs = {}) {
-      const round = await this._createRound(attrs);
-      await this._createGame(round);
+    createGame: async function() {
       this.transitionTo('games');
     }
-  },
-
-  _createRound: async function({ emojiPhrase }) {
-    const apollo = this.get('apollo');
-
-    const variables = {
-      emojiPhrase,
-      userId: this.currentUser.id
-    };
-
-    const roundResult = await apollo.mutate({
-      mutation: createRoundMutation,
-      variables
-    });
-
-    return roundResult.createRound;
-  },
-
-  _createGame: async function(round) {
-    const apollo = this.get('apollo');
-
-    const gameResult = await apollo.mutate({
-      mutation: createGameMutation,
-      variables: {
-        roundId: round.id,
-        userId: this.currentUser.id
-      }
-    });
-
-    return gameResult.createGame;
-  },
-
-  // typically we would fetch this from a currentUser service
-  currentUser: {
-    id: 'cj7c07dmx2mz70110ju7rb8dr'
   }
 });
